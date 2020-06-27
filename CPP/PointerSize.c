0bffec5e7d30cc993ea7bddcebaa8339455f4ffe
onlitstechnologies <61614449+onlitstechnologies@users.noreply.github.com>
2020-06-27 09:00:57 +0530

    Commit_20200627

---
+++ CPP/PointerSize.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	void *p;
+	
+	printf("The size of p is %d bytes.",sizeof(p));
+	
+	return 0;	
+}
+
+inline void sum()
+{
+	
+	
+}
---
+++ VB GUI/AdditionApp/AdditionApp/AdditionApp.vb
@@ -0,0 +1,3 @@
+﻿Public Class AdditionApp
+
+End Class
