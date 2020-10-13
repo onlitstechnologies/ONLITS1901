25707f43d9a865d10bbe10987d09dc9c193b2187
onlitstechnologies <onlitstechnologies@gmail.com>
2020-10-13 20:58:42 +0530

    20201013

--- GitPullPush.sh
+++ GitPullPush.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "fetching...\n"
 mess=$(date +'%Y%m%d')
 git pull https://onlitstechnologies@github.com/onlitstechnologies/ONLITS1901.git
+echo "\npushing...\n"
 git add .
 git commit -m "$mess"
 git push https://onlitstechnologies@github.com/onlitstechnologies/ONLITS1901.git
