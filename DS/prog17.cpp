33384e11036d990b23bddcb9e498dbbb820fb440
onlitstechnologies <onlitstechnologies@gmail.com>
2020-10-20 12:49:30 +0530

    20201030

---
+++ DS/prog17.cpp
@@ -0,0 +1,19 @@
+/*Program to traverse a linear array*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int no[] = {56, 76, 5, 12, 99, 33, 20, 1, 101, 88};
+	
+	cout<<"The array contains the following elements:"<<endl;
+	
+	for(int i=0; i<10; i++)
+	{
+		cout<<no[i]<<endl;
+	}
+	
+	return 0;
+}
