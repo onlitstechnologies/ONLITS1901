33b0c3ec4281581d4b73bcde97c3bf8bd922ecf6
onlitstechnologies <onlitstechnologies@gmail.com>
2021-06-12 13:10:09 +0530

    20210612

--- Projects/OLTP/NewOrderSubmit.php
+++ Projects/OLTP/NewOrderSubmit.php
@@ -1,9 +1,14 @@
 <?php
-include 'Connect.php';
-$item_id = $_POST['item_id'];
-$qty = $_POST['quantity'];
-$sql = "INSERT INTO orders(item_id, quantity) VALUES($item_id,$qty)";
-$con->query($sql);
-//echo 'Success!';
-echo "<script>alert('Data inserted successfully');</script>";
-echo "<script>window.location.href='NewOrder.html';</script>";
+try {
+    include 'Connect.php';
+    $item_id = $_POST['item_id'];
+    $qty = $_POST['quantity'];
+    $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO orders(item_id, quantity) VALUES($item_id,$qty)";
+    $con->exec($sql);
+    echo "<script>alert('Data inserted successfully');</script>";
+    echo "<script>window.location.href='NewOrder.html';</script>";
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+$con = null;
