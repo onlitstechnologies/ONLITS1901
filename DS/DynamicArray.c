560f9487ef47ee6d8b8249d8dfee5f3e91952bfa
onlitstechnologies <onlitstechnologies@gmail.com>
2021-06-17 09:56:22 +0530

    20210617

---
+++ DS/DynamicArray.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n, *arr;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+    arr = malloc(n * sizeof(int));
+    printf("\nEnter %d elements below:\n", n);
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("\nThe array elements are as follows:\n");
+    for(int i=0; i<n; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    int on = n;
+    printf("Enter new size for the array: ");
+    scanf("%d", &n);
+
+    //free(arr);
+
+    arr = realloc(arr, n * sizeof(n));
+
+
+    for(int i=on; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nThe array elements are as follows:\n");
+    for(int i=on; i<n; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+
+    printf("\nThe array elements are as follows:\n");
+    for(int i=0; i<n; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
