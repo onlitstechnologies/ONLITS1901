88923d6a5464914707b662538bec1999d82b21d7
onlitstechnologies <onlitstechnologies@gmail.com>
2021-07-03 10:00:11 +0530

    20210703

---
+++ DS/Java/LinkedListJFC.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class LinkedListJFC {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ArrayList<Integer> al = new ArrayList<>();
+    }
+}
