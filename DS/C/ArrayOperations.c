811f614448e998936a83f73f5d5546ca57e008a5
onlitstechnologies <onlitstechnologies@gmail.com>
2021-07-15 20:05:46 +0530

    20210715

--- DS/C/ArrayOperations.c
+++ DS/C/ArrayOperations.c
@@ -33,12 +33,13 @@ int main()
 
 void menu(int *arr, int *size)
 {
-    int ch;
-    do
+    int ch;       //O(1)
+    do            
     {
         printf("**************************** MAIN MENU **********************************\n");
         printf("1. Display (Traverse)\n");
         printf("2. Insert (at the end)\n");
+        printf("13. Delete (from the end)\n");
         printf("Enter your choice (0 to exit): ");
         scanf("%d", &ch);
         switch(ch)
@@ -52,6 +53,9 @@ void menu(int *arr, int *size)
             case 2:
                 insert_end(arr, size);
                 break;
+            case 13:
+                delete_end(arr, size);
+                break;
             default:
                 printf("\nInvalid option!\n\n");
         }
@@ -59,24 +63,37 @@ void menu(int *arr, int *size)
     
 }
 
-void display(int *arr, int *size)
+void display(int *arr, int *size)     //O(N) - linear
 {
-    if(*size == 0)
+    if(*size == 0)      //O(1)
     {
-        printf("\nThe array is empty. There is nothing to display!\n\n");
+        printf("\nThe array is empty. There is nothing to display!\n\n"); //O(1)
     }
-    else
+    else      //O(1)
     {
-        printf("\nThe elements of the array are:\n");
-        for(int i=0; i < *size; i++)
+        printf("\nThe elements of the array are:\n");     //O(1)
+        for(int i=0; i < *size; i++)      //O(N)
         {
-            printf("%d\n", arr[i]);
+            printf("%d\n", arr[i]);       //O(N)
         }
-        printf("\n");
+        printf("\n");         //O(1)
     }
 }
 
-void insert_end(int *arr, int *size)
+void insert_end(int *arr, int *size)    //O(1) - constant
 {
+    printf("Enter a number: ");     //O(1)
+    scanf("%d", &arr[*size]);       //O(1)
+    printf("\nItem inserted at index %d\n\n", *size);   //O(1)
+    (*size)++;  //O(1)
+}
 
+/*
+    O(1) + O(1) + O(1) + O(1)   =   O(4 X 1) =  O(1)
+*/
+
+void delete_end(int *arr, int *size)
+{
+    (*size)--;
+    printf("\nItem at index %d deleted.\n\n", *size);
 }
