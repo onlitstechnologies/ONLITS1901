cfb591f5ca250b82156c28f53a0a24971853efad
onlitstechnologies <onlitstechnologies@gmail.com>
2021-07-16 19:58:10 +0530

    20210716

--- DS/C/ArrayOperations.c
+++ DS/C/ArrayOperations.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MAX 100
 
@@ -24,76 +24,119 @@ void merge(int *arr1, int *size1, int *arr2, int *size2);
 
 int main()
 {
-    int size=0;
+    int size = 0;
     int *arr = malloc(MAX * sizeof(int));
     menu(arr, &size);
     return 0;
-
 }
 
 void menu(int *arr, int *size)
 {
-    int ch;       //O(1)
-    do            
+    int in;
+    int ch; //O(1)
+    do
     {
         printf("**************************** MAIN MENU **********************************\n");
         printf("1. Display (Traverse)\n");
         printf("2. Insert (at the end)\n");
+        printf("3. Insert (at a specified index)\n");
         printf("13. Delete (from the end)\n");
         printf("Enter your choice (0 to exit): ");
         scanf("%d", &ch);
-        switch(ch)
+        switch (ch)
         {
-            case 0:
-                exit(0);
-                break;
-            case 1:
-                display(arr, size);
-                break;
-            case 2:
-                insert_end(arr, size);
-                break;
-            case 13:
-                delete_end(arr, size);
-                break;
-            default:
-                printf("\nInvalid option!\n\n");
+        case 0:
+            exit(0);
+            break;
+        case 1:
+            display(arr, size);
+            break;
+        case 2:
+            insert_end(arr, size);
+            break;
+        case 3:
+            printf("Enter index: ");
+            scanf("%d", &in);
+            insert_index(arr, size, in);
+            break;
+        case 13:
+            delete_end(arr, size);
+            break;
+        default:
+            printf("\nInvalid option!\n\n");
         }
     } while (1);
-    
 }
 
-void display(int *arr, int *size)     //O(N) - linear
+void display(int *arr, int *size) //O(N) - linear
 {
-    if(*size == 0)      //O(1)
+    if (*size == 0) //O(1)
     {
         printf("\nThe array is empty. There is nothing to display!\n\n"); //O(1)
     }
-    else      //O(1)
+    else //O(1)
     {
-        printf("\nThe elements of the array are:\n");     //O(1)
-        for(int i=0; i < *size; i++)      //O(N)
+        printf("\nThe elements of the array are:\n"); //O(1)
+        for (int i = 0; i < *size; i++)               //O(N)
         {
-            printf("%d\n", arr[i]);       //O(N)
+            printf("%d\n", arr[i]); //O(N)
         }
-        printf("\n");         //O(1)
+        printf("\n"); //O(1)
     }
 }
 
-void insert_end(int *arr, int *size)    //O(1) - constant
+void insert_end(int *arr, int *size) //O(1) - constant
 {
-    printf("Enter a number: ");     //O(1)
-    scanf("%d", &arr[*size]);       //O(1)
-    printf("\nItem inserted at index %d\n\n", *size);   //O(1)
-    (*size)++;  //O(1)
+    if (*size == MAX)
+    {
+        printf("\nThe array is full. Cannot insert anymore!\n\n");
+    }
+    else
+    {
+        printf("Enter a number: ");                       //O(1)
+        scanf("%d", &arr[*size]);                         //O(1)
+        printf("\nItem inserted at index %d\n\n", *size); //O(1)
+        (*size)++;                                        //O(1)
+    }
 }
 
 /*
     O(1) + O(1) + O(1) + O(1)   =   O(4 X 1) =  O(1)
 */
 
-void delete_end(int *arr, int *size)
+void insert_index(int *arr, int *size, const int in)
 {
-    (*size)--;
-    printf("\nItem at index %d deleted.\n\n", *size);
+    if (*size == MAX)
+    {
+        printf("\nThe array is full. Cannot insert anymore!\n\n");
+    }
+    else if (in >= *size)
+    {
+        printf("\nIndex is out of range. Valid range is 0 to %d.\n\n", *size - 1);
+    }
+    else
+    {
+        for (int i = *size; i > in; i--)
+        {
+            arr[i] = arr[i - 1];
+        }
+
+        printf("Enter a number: ");
+        scanf("%d", &arr[in]);
+        printf("\nItem inserted at index %d\n\n", in);
+        (*size)++;
+    }
+}
+
+void delete_end(int *arr, int *size) //O(1)
+{
+    if (*size == 0) //O(1)
+    {
+        printf("\nThe array is empty. There is nothing to delete!\n\n"); //O(1)
+    }
+    else //O(1)
+    {
+        (*size)--;                                        //O(1)
+        printf("\nItem at index %d deleted.\n\n", *size); //O(1)
+    }
 }
